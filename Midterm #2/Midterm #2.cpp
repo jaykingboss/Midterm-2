5353f6d289a59571c4a863e760226c5d5e8313df
JaysonY <36421979+jaykingboss@users.noreply.github.com>
2024-10-17 20:45:04 -0700

    commit

--- Midterm #2/Midterm #2.cpp
+++ Midterm #2/Midterm #2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 const int NUM_CUSTOMERS = 5;    // Initial number of customers
@@ -8,7 +9,7 @@ const int MAX_NAMES = 30;        // Maximum number of names
 class DoublyLinkedList {
 private:
     struct Node {
-        string data;  
+        string data;
         Node* prev;
         Node* next;
         Node(string val, Node* p = nullptr, Node* n = nullptr) {
@@ -24,7 +25,47 @@ private:
 public:
     DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
-    
+    void push_back(string value) {
+        Node* newNode = new Node(value);
+        if (!tail) {
+            head = tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void push_front(string value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = tail = newNode;
+        }
+        else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void pop_front() {
+        if (!head) return;
+        Node* temp = head;
+        head = head->next;
+        if (head) head->prev = nullptr;
+        delete temp;
+        if (!head) tail = nullptr; 
+    }
+
+    void pop_back() {
+        if (!tail) return;
+        Node* temp = tail;
+        tail = tail->prev;
+        if (tail) tail->next = nullptr;
+        delete temp;
+        if (!tail) head = nullptr; 
+    }
 
     void print() {
         Node* current = head;
@@ -37,18 +78,31 @@ public:
             current = current->next;
         }
     }
+
+    bool is_empty() {
+        return head == nullptr;
+    }
+
+    Node* get_head() {
+        return head;
+    }
+
+    Node* get_tail() {
+        return tail;
+    }
 };
 
-void simulateCoffeeShop(string names[], int nameCount) {
-    DoublyLinkedList queue;
-    srand(1); 
+// Function to generate a random number between min and max
+int random(int min, int max) {
+    return (rand() % (max - min + 1)) + min;
 }
 
-int main() {
-    string names[MAX_NAMES] = {
-        "Jean", "Iris", "Omar", "Andy", "Drew",
-    };
-    int nameCount = 20;  // Number of names in the array
-    simulateCoffeeShop(names, nameCount);  // Run test
-    return 0;
+// Main simulation function
+void simulateCoffeeShop(string names[], int nameCount) {
+    DoublyLinkedList queue;
+    srand(1);  // random number 
+
+   
+    }
 }
+
